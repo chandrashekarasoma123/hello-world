b8ffdce52a82ce3476743dfa25aa5dcde9339acf
Somayaji <chandrashekara.somayaji@atos.net>
2022-02-18 21:46:24 +0530

    change on tf

--- main.tf
+++ main.tf
@@ -2,3 +2,5 @@ resource local_file name {
   filename     = "sample.txt"
   content      = " i love terraform"
   }
+resource "google_compute_instance" "kcs_server_inst" {
+  for_each = var.server_build_info
